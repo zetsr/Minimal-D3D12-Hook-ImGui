d56f2c3e1a97e50c92726162ce3524a1e5808644
zetsr <90092893+zetsr@users.noreply.github.com>
2025-12-24 00:57:16 +0800

    Delete hook.h

--- hook.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "global.h"
-
-HRESULT STDMETHODCALLTYPE hkPresent(IDXGISwapChain3* pSwapChain, UINT SyncInterval, UINT Flags);
-void STDMETHODCALLTYPE hkExecuteCommandLists(ID3D12CommandQueue* queue, UINT NumCommandLists, ID3D12CommandList* const* ppCommandLists);
-HRESULT STDMETHODCALLTYPE hkResizeBuffers(IDXGISwapChain* pSwapChain, UINT BufferCount, UINT Width, UINT Height, DXGI_FORMAT NewFormat, UINT SwapChainFlags);
-
-DWORD WINAPI MainThread(LPVOID);
